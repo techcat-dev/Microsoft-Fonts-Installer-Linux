8680872aec8b47fac3686deabc339c81fba24a7b
TechCat <143054835+techcat-dev@users.noreply.github.com>
2024-07-31 22:35:36 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+wget http://dl.techcat.dev/Misc/Microsoft-Fonts-Linux-TTF.zip
+unzip Microsoft-Fonts-Linux-TTF.zip
+cd Microsoft-Fonts-Linux-TTF
+./install-fonts-linux.sh
+cd ..
+rm -rf Microsoft-Fonts-Linux-TTF
+rm -rf Microsoft-Fonts-Linux-TTF.zip
+echo ""
+echo "Done!"
